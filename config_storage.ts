1973fec96c74a3f09ce31657f296a3c635b4c18f
Martin Tracey <matracey@outlook.ie>
2023-04-22 20:42:42 -0700

    docs(client): add JSDoc comments

--- config_storage.ts
+++ config_storage.ts
@@ -9,39 +9,91 @@ const GLOBAL_PROMPT_TEMPLATE_PATH = `${homedir()}/.commitgpt-template`;
 const LOCAL_PROMPT_TEMPLATE_PATH = `${process.cwd()}/.commitgpt-template`;
 
 interface Config {
+  /**
+   * The OpenAI API key.
+   */
   apiKey?: string;
+  /**
+   * The prompt template.
+   */
   promptTemplate?: string;
+  /**
+   * The OpenAI model to use.
+   *
+   * @example "text-davinci-003"
+   * @example "gpt-4"
+   *
+   * @see https://platform.openai.com/docs/models
+   */
   model: string;
+  /**
+   * The sampling temperature to use for the model, between 0 and 2.
+   *
+   * @example 0.5
+   */
   temperature: number;
+  /**
+   * The maximum number of tokens to generate in the completion.
+   *
+   * @example 2048
+   */
   maxTokens: number;
 }
 
+/**
+ * The default configuration.
+ */
 const defaultConfig = {
   model: "text-davinci-003",
   temperature: 0.5,
   maxTokens: 2048,
 } satisfies Config;
 
+/**
+ * Writes a JSON object to a file.
+ *
+ * @param path - The path of the file to write to.
+ * @param data - The data to write to the file.
+ */
 const writeJsonFile = (path: string, data: unknown) => {
   writeFileSync(path, JSON.stringify(data, null, 2));
 };
 
+/**
+ * Ensures that the global configuration file exists.
+ */
 function ensureGlobal() {
   if (!existsSync(GLOBAL_CONFIG_PATH)) {
     writeJsonFile(GLOBAL_CONFIG_PATH, {});
   }
 }
 
+/**
+ * Loads the global configuration file.
+ *
+ * @returns The global configuration file.
+ */
 function loadGlobal() {
   return JSON.parse(readFileSync(GLOBAL_CONFIG_PATH, "utf-8"));
 }
 
+/**
+ * Loads the local configuration file.
+ *
+ * @returns The local configuration file.
+ */
 function loadLocal(): Partial<Config> {
   if (!existsSync(LOCAL_CONFIG_PATH)) return {};
   return JSON.parse(readFileSync(LOCAL_CONFIG_PATH, "utf-8"));
 }
 
 let cache = null;
+
+/**
+ * Loads the configuration file.
+ *
+ * @returns The configuration file.
+ */
 function load() {
   if (cache) return cache;
   ensureGlobal();
@@ -51,6 +103,12 @@ function load() {
   return cache;
 }
 
+/**
+ * Asserts that the prompt template is valid.
+ *
+ * @param t - The prompt template to validate.
+ * @throws An error if the prompt template is invalid.
+ */
 function assertTempValid(t: string) {
   // should include {{diff}}
   if (!t.includes("{{diff}}")) {
@@ -58,6 +116,11 @@ function assertTempValid(t: string) {
   }
 }
 
+/**
+ * Loads the prompt template from the configuration file.
+ *
+ * @returns The prompt template.
+ */
 export function loadPromptTemplate(): string {
   if (existsSync(LOCAL_CONFIG_PATH)) {
     const temp = readFileSync(LOCAL_PROMPT_TEMPLATE_PATH, "utf-8");
@@ -76,11 +139,23 @@ export function loadPromptTemplate(): string {
   return defaultPromptTemplate;
 }
 
+/**
+ * Gets a configuration value from the configuration file.
+ *
+ * @param key - The key of the configuration value to get.
+ * @returns The configuration value.
+ */
 export function getConfig<T>(key: string): T {
   const config = load();
   return config[key];
 }
 
+/**
+ * Sets a global configuration value in the configuration file.
+ *
+ * @param key - The key of the configuration value to set.
+ * @param value - The value to set.
+ */
 export function setGlobalConfig(key: string, value: unknown) {
   const config = loadGlobal();
   config[key] = value;
--- index.ts
+++ index.ts
@@ -1,47 +1,43 @@
 #!/usr/bin/env node
-import { execSync } from "child_process";
 
+import { execSync } from "child_process";
 import enquirer from "enquirer";
 import ora from "ora";
 
 import { ChatGPTClient } from "./client.js";
 import { loadPromptTemplate } from "./config_storage.js";
 
-const debug = (...args: unknown[]) => {
+/**
+ * Logs debug information to the console if the DEBUG environment variable is set.
+ *
+ * @param args - The arguments to log.
+ */
+const debug = (...args: unknown[]): void => {
   if (process.env.DEBUG) {
     console.debug(...args);
   }
 };
 
+/**
+ * The string value for the custom message option in the commit message prompt.
+ */
 const CUSTOM_MESSAGE_OPTION = "[write own message]...";
+
+/**
+ * The spinner used to indicate that the tool is waiting for a response from the ChatGPT API.
+ */
 const spinner = ora();
 
-let diff = "";
-try {
-  diff = execSync("git diff --cached").toString();
-  if (!diff) {
-    console.log("No changes to commit.");
-    process.exit(0);
-  }
-} catch (e) {
-  console.log("Failed to run git diff --cached");
-  process.exit(1);
-}
-
-run(diff)
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((e: Error) => {
-    console.log("Error: " + e.message, e.cause ?? "");
-    process.exit(1);
-  });
-
-async function run(diff: string) {
+/**
+ * Runs the commit message prompt.
+ *
+ * @param diff - The diff to use in the prompt.
+ */
+async function runCommitMessagePrompt(diff: string): Promise<void> {
   // TODO: we should use a good tokenizer here
   const diffTokens = diff.split(" ").length;
   if (diffTokens > 2000) {
-    console.log(`Diff is way too bug. Truncating to 700 tokens. It may help`);
+    console.log(`Diff is way too big. Truncating to 700 tokens. It may help`);
     diff = diff.split(" ").slice(0, 700).join(" ");
   }
 
@@ -52,6 +48,29 @@ async function run(diff: string) {
     ["```", diff, "```"].join("\n")
   );
 
+  const message = await promptForCommitMessage(api, prompt);
+
+  if (message === CUSTOM_MESSAGE_OPTION) {
+    execSync("git commit", { stdio: "inherit" });
+  } else {
+    execSync(`git commit -m '${escapeCommitMessage(message)}'`, {
+      stdio: "inherit",
+    });
+  }
+}
+
+/**
+ * Prompts the user to select a commit message.
+ *
+ * @param api - The OpenAI API client to use.
+ * @param prompt - The prompt to use.
+ * @returns The selected commit message.
+ * @throws An error if the prompt fails or the user cancels the prompt.
+ */
+async function promptForCommitMessage(
+  api: ChatGPTClient,
+  prompt: string
+): Promise<string> {
   while (true) {
     debug("prompt: ", prompt);
     const choices = await getMessages(api, prompt);
@@ -64,15 +83,7 @@ async function run(diff: string) {
         choices,
       });
 
-      if (answer.message === CUSTOM_MESSAGE_OPTION) {
-        execSync("git commit", { stdio: "inherit" });
-        return;
-      } else {
-        execSync(`git commit -m '${escapeCommitMessage(answer.message)}'`, {
-          stdio: "inherit",
-        });
-        return;
-      }
+      return answer.message;
     } catch (e) {
       console.log("Aborted.");
       console.log(e);
@@ -81,7 +92,17 @@ async function run(diff: string) {
   }
 }
 
-async function getMessages(api: ChatGPTClient, request: string) {
+/**
+ * Gets a list of commit messages from the ChatGPT API.
+ *
+ * @param api - The ChatGPT API client.
+ * @param request - The request to send to the API.
+ * @returns A list of commit messages.
+ */
+async function getMessages(
+  api: ChatGPTClient,
+  request: string
+): Promise<string[]> {
   spinner.start("Asking ChatGPT 🤖 for commit messages...");
 
   // send a message and wait for the response
@@ -109,7 +130,13 @@ async function getMessages(api: ChatGPTClient, request: string) {
   }
 }
 
-function normalizeMessage(line: string) {
+/**
+ * Normalizes a commit message by removing unnecessary characters and whitespace.
+ *
+ * @param line - The commit message to normalize.
+ * @returns The normalized commit message.
+ */
+function normalizeMessage(line: string): string {
   return line
     .trim()
     .replace(/^(\d+\.|-|\*)\s+/, "")
@@ -121,6 +148,33 @@ function normalizeMessage(line: string) {
     .trim();
 }
 
-function escapeCommitMessage(message: string) {
+/**
+ * Escapes single quotes in a commit message by replacing them with two single quotes.
+ *
+ * @param message - The commit message to escape.
+ * @returns The escaped commit message.
+ */
+function escapeCommitMessage(message: string): string {
   return message.replace(/'/, `''`);
 }
+
+let diff = "";
+try {
+  diff = execSync("git diff --cached").toString();
+  if (!diff) {
+    console.log("No changes to commit.");
+    process.exit(0);
+  }
+} catch (e) {
+  console.log("Failed to run git diff --cached");
+  process.exit(1);
+}
+
+runCommitMessagePrompt(diff)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e: Error) => {
+    console.log("Error: " + e.message, e.cause ?? "");
+    process.exit(1);
+  });
