9e51e461495a5d3f81d73532fc8079d3caf383ad
Bruno Vinicius <57375055+brunicius@users.noreply.github.com>
2023-04-25 00:32:47 -0300

    🎨 style(client.ts): fix indentation and quotes The code was formatted to use double quotes instead of single quotes and to fix the indentation.

--- client.ts
+++ client.ts
@@ -1,34 +1,34 @@
 // Adapted from: https://github.com/wong2/chat-gpt-google-extension/blob/main/background/index.mjs
 
-import { createParser } from 'eventsource-parser';
-import { v4 as uuidv4 } from 'uuid';
-import ExpiryMap from 'expiry-map';
-import fetch, { Response } from 'node-fetch';
+import { createParser } from "eventsource-parser";
+import { v4 as uuidv4 } from "uuid";
+import ExpiryMap from "expiry-map";
+import fetch, { Response } from "node-fetch";
 
 export type ClientConfig = {
   sessionToken: string;
 };
 
-const KEY_ACCESS_TOKEN = 'accessToken';
+const KEY_ACCESS_TOKEN = "accessToken";
 const USER_AGENT =
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36";
 const cache = new ExpiryMap(10 * 1000);
 
 export async function refreshAccessToken(sessionToken: string) {
   if (cache.get(KEY_ACCESS_TOKEN)) {
     return cache.get(KEY_ACCESS_TOKEN);
   }
-  const resp = await fetch('https://chat.openai.com/api/auth/session', {
+  const resp = await fetch("https://chat.openai.com/api/auth/session", {
     headers: {
-      'User-Agent': USER_AGENT,
-      cookie: '__Secure-next-auth.session-token=' + sessionToken,
+      "User-Agent": USER_AGENT,
+      cookie: "__Secure-next-auth.session-token=" + sessionToken,
     },
   })
-    .then(r => r.json() as any)
+    .then((r) => r.json() as any)
     .catch(() => ({}));
 
   if (!resp.accessToken) {
-    throw new Error('Unauthorized');
+    throw new Error("Unauthorized");
   }
 
   cache.set(KEY_ACCESS_TOKEN, resp.accessToken);
@@ -36,7 +36,10 @@ export async function refreshAccessToken(sessionToken: string) {
 }
 
 export class ChatGPTClient {
-  constructor(public config: ClientConfig, public conversationId: string = uuidv4()) {}
+  constructor(
+    public config: ClientConfig,
+    public conversationId: string = uuidv4(),
+  ) {}
 
   async ensureAuth() {
     await refreshAccessToken(this.config.sessionToken);
@@ -44,32 +47,32 @@ export class ChatGPTClient {
   async getAnswer(question: string): Promise<string> {
     const accessToken = await refreshAccessToken(this.config.sessionToken);
 
-    let response = '';
+    let response = "";
     return new Promise((resolve, reject) => {
-      fetchSSE('https://chat.openai.com/backend-api/conversation', {
-        method: 'POST',
+      fetchSSE("https://chat.openai.com/backend-api/conversation", {
+        method: "POST",
         headers: {
-          'User-Agent': USER_AGENT,
-          'Content-Type': 'application/json',
+          "User-Agent": USER_AGENT,
+          "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify({
-          action: 'next',
+          action: "next",
           messages: [
             {
               id: uuidv4(),
-              role: 'user',
+              role: "user",
               content: {
-                content_type: 'text',
+                content_type: "text",
                 parts: [question],
               },
             },
           ],
-          model: 'text-davinci-002-render',
+          model: "text-davinci-002-render",
           parent_message_id: this.conversationId,
         }),
         onMessage: (message: string) => {
-          if (message === '[DONE]') {
+          if (message === "[DONE]") {
             return resolve(response);
           }
           const data = JSON.parse(message);
@@ -91,16 +94,17 @@ async function fetchSSE(resource, options) {
     (err as any).details = await resp.text(); // quick hack to persist the error details
     throw err;
   }
-  const parser = createParser(event => {
-    if (event.type === 'event') {
+  const parser = createParser((event) => {
+    if (event.type === "event") {
       onMessage(event.data);
     }
   });
 
-  resp.body.on('readable', () => {
+  resp.body.on("readable", () => {
     let chunk;
     while (null !== (chunk = resp.body.read())) {
       parser.feed(chunk.toString());
     }
   });
 }
+
